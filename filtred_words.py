7ba9ba0e5cde64b95e1819c95c13ffc7aedb4d8d
Bassim <bassim.matar@gmail.com>
2016-05-27 19:21:12 +0200

    Update and rename top_ten_words.py to filtred_words.py

---
+++ filtred_words.py
@@ -0,0 +1,34 @@
+from _textable.widgets.LTTL.Segmenter import Segmenter
+from _textable.widgets.LTTL.Input import Input
+
+def main():
+    global out_object
+    
+    unsignifiant = []
+    
+    unsignifiant_fr = [
+        'de', 'des', 'du', 'un', 'une', 'le', 'la', 'les', 'd', 'l', 'a', 'ou', 'dans', 'par', 'ce', 'cette'
+        '-', 'et', '', 'en', 'au', 'sur', 'pour', u'\xe0',
+        'est',
+        '!', '.', '?', ','
+    ]
+    
+    unsignifiant_wiki_fr = ['code', 'modifier']
+    
+    unsignifiant.extend(unsignifiant_fr)
+    unsignifiant.extend(unsignifiant_wiki_fr)
+    
+    filtredInputs = []
+ 
+    for segment in in_object:
+        word = segment.get_content()
+        if word.lower() not in unsignifiant:
+            filtredInputs.append(Input(word, label=False))
+        
+    segmenter = Segmenter() 
+    out_object = segmenter.concatenate(filtredInputs)
+    print out_object.to_string()
+            
+if __name__ == "__builtin__":
+    if in_object:
+        main()
